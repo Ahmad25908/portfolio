937f2ab267d6548aec2df4a9b80ef92226fb782b
Ahmad25908 <ahmadhassan41093@gmail.com>
2024-10-09 11:41:48 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Hero from "@/components/hero"
+// import Dark from "@/components/dark"?
+// Components
+function Home() {
+  return (
+    <>
+    <main>
+       <Hero/>
+      {/* <Dark /> */}
+
+
+
+
+
+    </main>
+      
+    
+    
+    
+    </>
+  )
+}
+
+export default Home
